eefcd5cfe076d8297bd61589eed07e5cf1abeda2
Alexis Roche <alexis.roche@gmail.com>
2013-08-19 09:51:37 +0200

    improve display in example

--- examples/_display.py
+++ examples/_display.py
@@ -2,29 +2,32 @@ import numpy as np
 import pylab as plt
 
 
-def display_fit(xs, target, method, color=None, linefmt=None, acronym=None, xmax=None):
+def display_fit(xs, target, method, color=None, acronym=None):
     if not hasattr(method, '__iter__'):
         method = (method, )
         if not color is None:
             color = (color, )
-        if not linefmt is None:
-            linefmt = (linefmt, )
         if not acronym is None:
             acronym = (acronym, )
     if not xs.shape[0] == 1:
         raise ValueError('univariate data expected')
-    if xmax == None:
-        xmax = int(np.max(np.abs(xs.squeeze()))) + 1
+    xmax = int(np.max(np.abs(xs.squeeze()))) + 1
     x = np.linspace(-xmax, xmax, 2 * xmax / 0.01)
     x = np.reshape(x, (1, x.size))
-    fits = [m(x) for m in method]
-    for fit, col, fmt in zip(fits, color, linefmt):
-        plt.plot(x.squeeze(), fit, fmt, color=col, linewidth=2)
+    fits = [m.fit(x) for m in method]
+    if color is None:
+        for fit in fits:
+            plt.plot(x.squeeze(), fit, linewidth=2)
+    else:
+        for fit, col in zip(fits, color):
+            plt.plot(x.squeeze(), fit, col, linewidth=2)
     if not acronym is None:
         plt.legend(acronym)
     target_xs = np.exp(target(xs.squeeze()))
     target_x = np.exp(target(x.squeeze()))
-    plt.stem(xs.squeeze(), target_xs, linefmt='k-', markerfmt='ko')
+    plt.stem(xs.squeeze(), target_xs, linefmt='k-', markerfmt='ko',
+             basefmt='k-')
     plt.plot(x.squeeze(), target_x, 'k')
-    plt.plot((-xmax, xmax), (0, 0), 'k')
+    x0, x1, y0, y1 = plt.axis()
+    plt.plot((x0, x1), (0, 0), 'k')
     plt.show()
--- examples/exp_power_law.py
+++ examples/exp_power_law.py
@@ -11,7 +11,6 @@ BETA = 2
 NPTS = 5
 DV = 4
 DM = -2
-DETERMINISTIC = True
 
 
 def gauss_hermite_rule(npts, mk, vk):
@@ -20,7 +19,6 @@ def gauss_hermite_rule(npts, mk, vk):
     with the normalized Gaussian N(0, h2) as a weighting function.
     """
     x, w = h_roots(npts)
-    x = x.real
     x *= np.sqrt(2 * vk)
     x += mk
     w /= np.sqrt(np.pi)
@@ -41,34 +39,26 @@ f_l = vs.fit('l')
 f_gp = vs.fit('gp', var=v)
 
 # Deterministic sampling approch (tweak a vs object)
-if DETERMINISTIC:
-    x, w = gauss_hermite_rule(NPTS, mk, vk)
-    vsd = VariationalSampler(target, (mk, vk), NPTS, x=x, w=w)
-    fd_kl = vsd.fit()
-    fd_l = vsd.fit('l')
-    fd_gp = vsd.fit('gp', var=v)
+x, w = gauss_hermite_rule(NPTS, mk, vk)
+vsd = VariationalSampler(target, (mk, vk), NPTS, x=x, w=w)
+fd_kl = vsd.fit()
+fd_l = vsd.fit('l')
+fd_gp = vsd.fit('gp', var=v)
 
 
-print('Error for IS: %f (expected: %f)'\
-           % (gs_fit.kl_div(f_l.fit), f_l.kl_error))
 print('Error for VS: %f (expected: %f)'\
           % (gs_fit.kl_div(f_kl.fit), f_kl.kl_error))
+print('Error for IS: %f (expected: %f)'\
+           % (gs_fit.kl_div(f_l.fit), f_l.kl_error))
 print('Error for BMC: %f' % gs_fit.kl_div(f_gp.fit))
+print('Error for GH: %f' % gs_fit.kl_div(fd_l.fit))
+print('Error for VSd: %f' % gs_fit.kl_div(fd_kl.fit))
+print('Error for GP: %f' % gs_fit.kl_div(fd_gp.fit))
 
-if DETERMINISTIC:
-    print('Error for GH: %f' % gs_fit.kl_div(fd_l.fit))
-    print('Error for VSd: %f' % gs_fit.kl_div(fd_kl.fit))
-    print('Error for GP: %f' % gs_fit.kl_div(fd_gp.fit))
-
-
-colors = ('black', 'gray', 'black')
-linefmt = ('-', '-', ':')
-xmax = int(np.max(np.abs(vsd.x.squeeze()))) + 1
-
+acronyms = ('VS', 'IS', 'BMC')
+colors = ('blue', 'red', 'green')
+legend = ('VS', 'direct', 'spline')
 plt.figure()
-display_fit(vs.x, target, (f_kl.fit, f_l.fit, f_gp.fit),
-            colors, linefmt, ('VS', 'IS', 'BMC'), xmax=xmax)
+display_fit(vs.x, target, (f_kl, f_l, f_gp), colors, legend)
 plt.figure()
-if DETERMINISTIC:
-    display_fit(vsd.x, target, (fd_kl.fit, fd_l.fit, fd_gp.fit),
-                colors, linefmt, ('VS', 'GH', 'GP'), xmax=xmax)
+display_fit(vsd.x, target, (fd_kl, fd_l, fd_gp), colors, legend)
